41a06cdedd5bc06874c04902b918d80d356f7280
ksingh7 <karan.singh731987@gmail.com>
2016-09-13 17:24:56 +0300

    script update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,5 @@
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y ansible vim
 yum remove -y epel-release
+useradd ceph
+echo "ceph:RedHat16" | chpasswd
