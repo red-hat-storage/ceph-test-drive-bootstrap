0f278a708a0a2ff21e81f520760ce3aa3632d007
ksingh7 <karan.singh731987@gmail.com>
2017-04-24 15:08:32 +0300

    added setup automation

--- env-setup/automated-setup/setup.sh
+++ env-setup/automated-setup/setup.sh
@@ -8,20 +8,19 @@ wget $URL/automated-setup/module-4.sh
 sudo yum install -y ceph-ansible wget
 sudo mv /etc/ansible/hosts /etc/ansible/hosts-default.bkp
 sudo wget -O /etc/ansible/hosts $URL/automated-setup/ansible-hosts
-
-echo "[defaults]" >> /home/ceph/.ansible.cfg
-echo "host_key_checking = False" >> /home/ceph/.ansible.cfg
-ansible all -m ping
-
-read -p "Press enter to continue"
-
-mkdir /home/ceph/ceph-ansible-keys
-
 sudo wget $URL/rhcs2_0/all -O /usr/share/ceph-ansible/group_vars/all
 sudo wget $URL/rhcs2_0/osds -O /usr/share/ceph-ansible/group_vars/osds
 sudo wget $URL/rhcs2_0/rgws -O /usr/share/ceph-ansible/group_vars/rgws
 
+echo "[defaults]" >> /home/ceph/.ansible.cfg
+echo "host_key_checking = False" >> /home/ceph/.ansible.cfg
+mkdir /home/ceph/ceph-ansible-keys
+
 sudo cp /usr/share/ceph-ansible/site.yml.sample /usr/share/ceph-ansible/site.yml
+
+ansible all -m ping
+
+read -p "Press enter to continue"
 
 echo "To Install RHCS Run ==> ansible-playbook /usr/share/ceph-ansible/site.yml -u ceph"
 echo "For Module-2 Run ==> sh module-2.sh"
