6e6a696fc7d21736ef9aeb499312fdcf06e80921
karan singh <karan.singh731987@gmail.com>
2017-04-25 19:42:12 +0300

    Update module-4.sh

--- env-setup/automated-setup/module-4.sh
+++ env-setup/automated-setup/module-4.sh
@@ -1,4 +1,10 @@
 #/bin/bash
-
+echo "==============================="
+echo "Creating RGW user for S3 access"
+echo "==============================="
 ssh rgw-node1 -t  radosgw-admin user create --uid='user1' --display-name='First User' --access-key='S3user1' --secret-key='S3user1key'
+
+echo "======================================"
+echo "Creating RGW subuser for swift access"
+echo "======================================"
 ssh rgw-node1 -t radosgw-admin subuser create --uid='user1' --subuser='user1:swift' --secret-key='Swiftuser1key' --access=full
